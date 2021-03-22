611814425d360cd55568c1e1605ea3c70f4f753b
Albert <ishengomaalbert@gmail.com>
2021-03-22 07:09:44 +0300

    Updates

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//Select DOM items
+const faq = document.querySelector('.faq');
+
+//Set initial state of the content
+let showContent = false;
+
+//Add Event Listener
+faq.addEventListener('click' , toggleContent);
+
+function toggleContent(e)
+ {
+     if(e.target.parentElement.classList.contains('arrow'))
+      {
+        if(!showContent)
+        {
+            e.target.classList.add('invert');
+            e.target.parentElement.previousElementSibling.classList.add('bold');
+            e.target.parentElement.parentElement.nextElementSibling.classList.add('show');
+           
+            //Set State
+            showContent =true
+        }
+
+        else
+        {
+            e.target.classList.remove('invert');
+            e.target.parentElement.previousElementSibling.classList.remove('bold');
+            e.target.parentElement.parentElement.nextElementSibling.classList.remove('show');
+
+            //Set State
+            showContent =false
+        }
+      }
+ 
+ }
